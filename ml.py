96796919bbde09ef9b2d04304ebb843e6389fed7
daniel <daniel@arilot.com>
2019-09-06 18:26:47 +0300

    fix deterministic results

--- ml.py
+++ ml.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 import logging
 import datetime
@@ -5,37 +6,26 @@ import settings
 
 RANDOM_SEED = 1234
 
-import os
-
 os.environ['PYTHONHASHSEED'] = str(RANDOM_SEED)
-
-# 2. Set `python` built-in pseudo-random generator at a fixed value
 import random
 
 random.seed(RANDOM_SEED)
 
-# 3. Set `numpy` pseudo-random generator at a fixed value
 import numpy as np
 
 np.random.seed(RANDOM_SEED)
+np.set_printoptions(precision=4)
 
-# 4. Set `tensorflow` pseudo-random generator at a fixed value
 import tensorflow as tf
 
+tf.reset_default_graph()
 tf.set_random_seed(RANDOM_SEED)
-
-# 5. Configure a new global `tensorflow` session
-from keras import backend as K
 
 session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
 sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
-K.set_session(sess)
+tf.keras.backend.set_session(sess)
+tf.set_random_seed(RANDOM_SEED)
 
-from tensorflow.python import keras
-from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.callbacks import TensorBoard
-from tensorflow.python.keras.initializers import glorot_uniform
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from tensorflow.python.keras.saving import model_from_json
 from datamanager import DataManager
 
@@ -58,22 +48,22 @@ class ML(DataManager):
     def __get_optimizer(self):
         return 'Adam'
 
-    def save_model(self, file_name):
+    def save_model(self, model, file_name):
         """
         Save model to local file
         """
 
-        if not self.model:
+        if not model:
             raise ModelNotLoaded('Please load model user "load_model" method')
 
         model_path = self.get_model_path(file_name)
         model_path_weights = os.path.join(model_path, file_name)
         model_path_json = os.path.join(model_path, 'model.json')
 
-        self.model.save_weights(model_path_weights, save_format='tf')
+        model.save_weights(model_path_weights, save_format='tf')
 
         with open(model_path_json, "w") as json_file:
-            json_file.write(self.model.to_json())
+            json_file.write(model.to_json())
 
         logging.info('Model saved into {model_path}'.format(model_path=model_path))
         return model_path
@@ -101,11 +91,7 @@ class ML(DataManager):
         return model
 
     def __get_image_data_generator(self):
-        return ImageDataGenerator(
-            rescale=1. / 255,
-            shear_range=0.2,
-            zoom_range=0.2,
-            horizontal_flip=True)
+        return tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255)
 
     def __get_callbacks(self):
         # Define the Keras TensorBoard callback.
@@ -113,7 +99,7 @@ class ML(DataManager):
         if settings.TENSORBOARD_LOGS_ENABLED:
             self.makedirs(self.LOG_DIR)
             logdir = os.path.join(self.LOG_DIR, datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
-            tensorboard_callback = TensorBoard(log_dir=logdir)
+            tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
             callbacks.append(tensorboard_callback)
         return callbacks
 
@@ -143,49 +129,49 @@ class ML(DataManager):
 
         classes_count = len(train_generator.class_indices.keys())
 
-        self.model = Sequential()
-        self.model.add(keras.layers.Convolution2D(filters=56, kernel_size=(3, 3), activation='relu',
-                                                  input_shape=train_generator.image_shape,
-                                                  kernel_initializer=glorot_uniform(seed=RANDOM_SEED)))
-        # self.model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(keras.layers.Convolution2D(32, (3, 3), activation='relu',
-                                                  kernel_initializer=glorot_uniform(seed=RANDOM_SEED)))
-        # self.model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(keras.layers.Flatten())
-        self.model.add(keras.layers.Dense(units=64, activation='relu',
-                                          kernel_initializer=glorot_uniform(seed=RANDOM_SEED)))
-        self.model.add(keras.layers.Dense(units=classes_count, activation='softmax',
-                                          kernel_initializer=glorot_uniform(seed=RANDOM_SEED)))
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Convolution2D(filters=56, kernel_size=(3, 3), activation='relu',
+                                                input_shape=train_generator.image_shape,
+                                                kernel_initializer=tf.keras.initializers.zeros()))
+        # model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+        model.add(tf.keras.layers.Convolution2D(32, (3, 3), activation='relu',
+                                                kernel_initializer=tf.keras.initializers.zeros()))
+        # model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(units=64, activation='relu',
+                                        kernel_initializer=tf.keras.initializers.zeros()))
+        model.add(tf.keras.layers.Dense(units=classes_count, activation='softmax',
+                                        kernel_initializer=tf.keras.initializers.zeros()))
 
-        self.model.compile(optimizer=self.__get_optimizer(), loss='categorical_crossentropy',
-                           metrics=['accuracy'])
+        model.compile(optimizer=self.__get_optimizer(), loss='categorical_crossentropy',
+                      metrics=['accuracy'])
 
         steps_per_epoch = round(train_size) // settings.BATCH_SIZE
         validation_steps = round(validate_size) // settings.BATCH_SIZE
 
-        self.model.fit_generator(train_generator,
-                                 epochs=settings.EPOCHS,
-                                 steps_per_epoch=steps_per_epoch,
-                                 validation_data=validation_generator,
-                                 validation_steps=validation_steps,
-                                 shuffle=False,
-                                 max_queue_size=1,
-                                 callbacks=self.__get_callbacks())
+        model.fit_generator(train_generator,
+                            epochs=settings.EPOCHS,
+                            steps_per_epoch=steps_per_epoch,
+                            validation_data=validation_generator,
+                            validation_steps=validation_steps,
+                            shuffle=False,
+                            max_queue_size=1,
+                            callbacks=self.__get_callbacks())
 
-        self.model.summary()
+        model.summary()
 
         logging.info('Classes: {classes}'.format(classes="; ".join(
             ['%s:%s' % (i, train_generator.class_indices[i]) for i in train_generator.class_indices.keys()])))
 
-        loss, acc = self.model.evaluate(validation_generator)
+        loss, acc = model.evaluate(validation_generator)
         print("Untrained model, accuracy: {:5.2f}%".format(100 * acc))
 
-        return self.save_model(model_uri)
+        return self.save_model(model, model_uri)
 
     async def inference(self, image_url, model_uri, output_uri):
         self.makedirs([self.TMP_DIR])
 
-        self.model = self.load_model(model_uri)
+        model = self.load_model(model_uri)
 
         image_tmp_path = os.path.join(self.TMP_DIR, uuid.uuid1().__str__() + ".jpg")
 
@@ -194,13 +180,13 @@ class ML(DataManager):
             "file_path": image_tmp_path
         })
 
-        img = load_img(image_tmp_path,
-                       target_size=(settings.IMAGE_SIZE, settings.IMAGE_SIZE))
-        img = img_to_array(img)
+        img = tf.keras.preprocessing.image.load_img(image_tmp_path,
+                                                    target_size=(settings.IMAGE_SIZE, settings.IMAGE_SIZE))
+        img = tf.keras.preprocessing.image.img_to_array(img)
         img = np.reshape(img, [settings.IMAGE_SIZE, settings.IMAGE_SIZE, 3])
         img = np.expand_dims(img, axis=0)
 
-        result = self.model.predict_classes(img)
+        result = model.predict_classes(img)
 
         print(result)
 
@@ -209,7 +195,7 @@ class ML(DataManager):
         return result
 
     async def evaluate(self, model_filename):
-        self.model = self.load_model(model_filename)
+        model = self.load_model(model_filename)
 
         validation_generator = self.__get_image_data_generator().flow_from_directory(
             self.VALIDATE_DIR,
@@ -217,5 +203,5 @@ class ML(DataManager):
             batch_size=settings.BATCH_SIZE,
             class_mode='categorical')
 
-        loss, categorical_accuracy, acc = self.model.evaluate(validation_generator, use_multiprocessing=True)
+        loss, categorical_accuracy, acc = model.evaluate(validation_generator, use_multiprocessing=True)
         print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
