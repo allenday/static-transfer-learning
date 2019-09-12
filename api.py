62c543b138d8f270212e0865f91c0ce27ee445c7
Daniel Yavorovych <daniel@arilot.com>
2019-09-12 14:13:59 +0300

    * add rest API * modify readme * remove deprecated scripts * remove evaluate mode * enable local model caching * migrate to base docker image "python:3" * add docker-compose * remove deprecated tensorflow config file

--- api.py
+++ api.py
@@ -1,5 +1,8 @@
+import asyncio
+import concurrent.futures
 import json
 import settings
+import logging
 from aiojobs.aiohttp import setup, spawn
 from aiohttp.client_exceptions import InvalidURL
 from ml import ML, ModelNotFound, ErrorDownloadImage, ErrorProcessingImage
@@ -7,6 +10,16 @@ from aiohttp import web
 from aiohttp_swagger import setup_swagger
 
 m = ML()
+
+
+def run(corofn, *args):
+    loop = asyncio.new_event_loop()
+    try:
+        coro = corofn(*args)
+        asyncio.set_event_loop(loop)
+        return loop.run_until_complete(coro)
+    finally:
+        loop.close()
 
 
 async def train(request):
@@ -47,7 +60,10 @@ async def train(request):
     status = m.get_model_status(model_name)
 
     if status == m.NOT_FOUND:
-        await spawn(request, m.train(**data))
+        # spawn(request, m.train(**data))
+        loop = asyncio.get_event_loop()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            await loop.run_in_executor(executor, run, m.train, data['csv_url'], data.get('model_url'))
         status = m.NEW
 
     return web.Response(body=json.dumps({
@@ -113,11 +129,13 @@ async def inference(request):
     return web.Response(body=json.dumps(result))
 
 
+logging.basicConfig(level=logging.INFO)
+
 app = web.Application()
 app.router.add_route('POST', "/train", train)
 app.router.add_route('POST', "/inference", inference)
 
-setup(app, limit=10000)
+setup(app, limit=None, pending_limit=None)
 setup_swagger(app)
 
 web.run_app(app, host=settings.API_HOST, port=settings.API_PORT)
